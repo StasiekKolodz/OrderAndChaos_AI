62570abc66788aa0ee0e165e86917803e902f2cd
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-09-12 23:26:00 +0200

    developing trainer

--- nn_model.py
+++ nn_model.py
@@ -5,6 +5,10 @@ from torch import nn
 class Network(nn.Module):
     def __init__(self):
         super().__init__()
+        """Input of the NN is board content flatened and output is 72 neurons for 
+        every field (36) and every sign on that field(36*2=72)
+
+        Firstly all fields and x sign next all fields and o sign"""
         self.lin1 = nn.Linear(36, 100)
         self.relu = nn.ReLU()
         self.lin2 = nn.Linear(100, 72)
@@ -15,13 +19,16 @@ class Network(nn.Module):
         return x
 
 class Trainer:
-    def __init__(self, model, lr, gamma):
+    def __init__(self, model, lr=0.01):
         self.lr = lr
         self.gamma = gamma
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.CrossEntropyLoss()
 
+    def set_lr(self, lr):
+        self.lr = lr
+        
     def train_step(self, states, actions, reward):
 
         self.optimizer.zero_grad()
