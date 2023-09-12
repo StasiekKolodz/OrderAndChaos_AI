a0339b55b61041f1acc4200c3fb924b91bedb59f
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-09-12 23:51:40 +0200

    added player trainer

--- AiPlayer.py
+++ AiPlayer.py
@@ -9,6 +9,7 @@ class AiPlayer(PcRandomPlayer):
         super().__init__(self, name, player_goal)
         self.model = Network()
 
+
     def decode_sign(self, sign):
         if sign == '-':
             return 0
@@ -40,8 +41,31 @@ class AiPlayer(PcRandomPlayer):
         column = (max_pred%36)%6
         return row, column, sign
      
-class OrderTrainer(AiPlayer):
-    def __init__(self, name, player_goal):
-        self.trainer = Trainer
 
+class PlayerTrainer(AiPlayer):
+    def __init__(self, name, goal):
+        self.name = name
+        self.goal = goal
+        self.trainer = Trainer(self.model)
+
+        self.board_states = []
+        self.moves = []
+
+    def set_trainer_lr(self, lr):
+        self.trainer.set_lr(lr)
+
+    def generate_move(self, board_combinations):
+        self.board_states.append(board_combinations.state())
+
+        super().generate_move(board_combinations)
+        self
+class OrderTrainer(AiPlayer):
+    def __init__(self):
+        super().__init__('Order Trainer', 'order')
+
+
+
+class ChaosTrainer(AiPlayer):
+    def __init__(self):
+        super().__init__('Chaos Trainer', 'chaos')
 
