f242c279ccfbc7d784ba3c9b818467fb73513b68
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-10-24 17:59:48 +0200

    .

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import time
 def main():
     start = time.time()
     training_game = TrainingGame()
-    training_game.training_session(10000)
+    training_game.training_session(100)
     end = time.time()   
     print(f"Total training seesion time: {end-start}")
 
